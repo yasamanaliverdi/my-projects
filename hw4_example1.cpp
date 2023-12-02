32e48163df8f0b8f66f96f3b2aa926142317dbd8
yasamanaliverdi <52723106+yasamanaliverdi@users.noreply.github.com>
2023-12-03 02:31:40 +0330

    Add files via upload

---
+++ hw4_example1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+main () {
+	
+   float x,y;
+	char op;
+	cout<<"please enter your expression :";
+	cin>>x>>op>>y;
+   
+    switch(op){
+
+      
+		case '+'  :
+			cout<<"the result is:"<<x+y;
+			
+			break ; 
+			
+		case '-'	:
+			 cout<<"the result is:"<<x-y;
+		
+			break ;
+			
+		case'*'	:
+			 cout<<"the result is:"<<x*y;
+		
+			break ;
+			
+		case '/' :
+			 cout<<"the result is"<<x/y;
+			
+			break ;
+			
+	
+		
+		
+			 
+			
+			
+			
+}
+
+}
+	
+	
+
+
+
+
+
