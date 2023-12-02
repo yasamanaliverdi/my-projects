24be7729de2395398981b84841e8c2aae86b4ec2
yasamanaliverdi <52723106+yasamanaliverdi@users.noreply.github.com>
2023-12-03 02:22:06 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	int n,m;
+	int p=1;
+	
+	cout<<"please enter your first number:";
+	cin>>n;
+	
+	cout<<"please enter your second number:";
+	cin>>m;
+	
+    while (m>0){
+	p=p*n;
+	m=m-1;
+	}
+	cout<<"p:"<<p;
+ 
+	
+	}
+
+	
