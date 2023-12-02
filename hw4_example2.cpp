3983828eb64ed3adfea4e650662bf1d524e95c8c
yasamanaliverdi <52723106+yasamanaliverdi@users.noreply.github.com>
2023-12-03 02:32:04 +0330

    Add files via upload

---
+++ hw4_example2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+ using namespace std;
+ main(){ 
+ float x,y,z,h,f,m,g,b,c,u;
+ cout<<"please enter your x:";
+ cin>>x;
+ cout<<"please enter your y:";
+ cin>>y;
+ cout<<"please enter your z:";
+ cin>>z;
+ h=x-y;
+ f=x+y;
+ m=h*f;
+ g=x*y*z;
+ b=g*g;
+ c=b++;
+ u=m/c;
+ cout<<"your answer is:"<<u;
+ 
+ }
