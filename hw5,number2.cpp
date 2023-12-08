8a250b3a2cdb292258cc5e2bfb82899cb85a7fdd
yasamanaliverdi <52723106+yasamanaliverdi@users.noreply.github.com>
2023-12-08 22:55:50 +0330

    Add files via upload

---
+++ hw5,number2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+ main() {
+ 	char alpabet ;
+ 	
+ 	cout<<"please enter your  letter:"<<alpabet;
+ 	cin>>alpabet;
+ 	switch(alpabet){
+ 		case'a':
+		 case'e':
+		 case'i':
+		 case'u':
+		 case'o':
+		 
+ 		cout<<"your letter is vowel";
+ 		break;
+ 		
+  case'b':
+  case'c':
+  case'd':
+  case'f':
+  case'g':
+  case'h':
+  case'j':
+  case'k':
+  case'l':
+  case'm':
+  case'n':
+  case'p':
+  case'q':
+  case'r':
+  case's':
+  case't':
+  case'w':
+  case'x':
+  case'y':
+  case'z':
+  case'v':
+  	
+		
+ 			cout<<"your letter is not vowel";
+ 		}
+ 		
+ 		
+	 }
