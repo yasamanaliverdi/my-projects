d0971549da56d8ba70f77d9da90f1d144c09fab9
yasamanaliverdi <52723106+yasamanaliverdi@users.noreply.github.com>
2024-01-16 14:48:24 +0330

    Add files via upload

---
+++ hw7.example3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+# include<cmath>
+using namespace std;
+
+int main() {
+	int x,y;
+	cin>>x;
+	int p=0;
+	for (y=1;y<=x;y++){
+		if(x%y==0){
+	
+        p+=y;
+}
+	
+			
+	}
+cout<<p;
+}
